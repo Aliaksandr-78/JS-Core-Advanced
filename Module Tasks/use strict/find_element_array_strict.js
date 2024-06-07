3400cb120bd9def536a6c8e369d6c3e24758ff8c
Aliaksandr-78 <107933930+Aliaksandr-78@users.noreply.github.com>
2024-06-07 10:08:23 +0300

    Update find_element_array_strict.js

--- Module Tasks/use strict/find_element_array_strict.js
+++ Module Tasks/use strict/find_element_array_strict.js
@@ -1,5 +1,8 @@
 'use strict'
 function task(arr, meaning){
+    if (arr.length === 0 || meaning === undefined || meaning === null) {
+        return false
+    }
     for (let i = 0; i<arr.length; i++){
         if (arr[i] === meaning){
             return true
